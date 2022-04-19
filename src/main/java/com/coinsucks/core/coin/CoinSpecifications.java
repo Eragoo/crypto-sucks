69894b5cfbf3f4cb74a422bb8d42dba773be078c
Yevhenii Kukhol <y.kukhol@spd-ukraine.com>
2022-04-19 16:03:19 +0300

    Fixed case

--- src/main/java/com/coinsucks/core/coin/CoinSpecifications.java
+++ src/main/java/com/coinsucks/core/coin/CoinSpecifications.java
@@ -6,6 +6,10 @@ import static org.apache.commons.lang3.StringUtils.isBlank;
 
 public class CoinSpecifications {
     public static Specification<Coin> byName(String name) {
-        return (r, cq, cb) -> isBlank(name) ? null : cb.like(cb.upper(r.get(Coin_.name)), "%"+name.toUpperCase()+"%");
+        return (r, cq, cb) -> isBlank(name) ? null :
+                cb.or(
+                        cb.like(cb.upper(r.get(Coin_.name)), "%"+name.toUpperCase()+"%"),
+                        cb.like(cb.upper(r.get(Coin_.symbol)), "%"+name.toUpperCase()+"%")
+                );
     }
 }
